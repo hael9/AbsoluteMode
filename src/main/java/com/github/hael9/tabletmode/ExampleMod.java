8a8ed10427746d47be74464771fd70e5678fe6ef
GitHub Action <action@github.com>
2023-10-31 14:10:58 +0000

    Initialize template repository proper
    Set package name to com.github.hael9.tabletmode
    Set project name to TabletMode
    Set modid to tabletmode

--- src/main/java/com/github/hael9/tabletmode/ExampleMod.java
+++ src/main/java/com/github/hael9/tabletmode/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.hael9.tabletmode;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "tabletmode", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
